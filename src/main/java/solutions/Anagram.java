0b32bb0b0608957061c25ebd01aa4bf1f98963fa
Ehsan <Ehsanghajari1989@gmail.com>
2023-06-06 01:34:53 -0400

    First two problems

--- src/main/java/solutions/Anagram.java
+++ src/main/java/solutions/Anagram.java
@@ -1,2 +1,40 @@
-package solutions;public class Anagram {
+package solutions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Anagram {
+    public static boolean isAnagram(String s, String t) {
+        if (countCharacters(s) == countCharacters(t) &&
+                countCharactersRepeats(s).equals(countCharactersRepeats(t))) {
+            return true;
+        }
+        return false;
+    }
+
+    public static Map<Character, Integer> countCharactersRepeats(String inputString) {
+        Map<Character, Integer> charsMap = new HashMap<>();
+        for (char ch : inputString.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                ch = Character.toLowerCase(ch);
+                charsMap.put(ch, charsMap.getOrDefault(ch, 0) + 1);
+            }
+        }
+        return charsMap;
+    }
+
+    public static int countCharacters(String input) {
+        int charCount = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (Character.isLetter(ch)) {
+                charCount++;
+            }
+        }
+        return charCount;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("sdaf  12", "asdf5435"));
+    }
 }
--- src/main/java/solutions/ContainsDuplicate.java
+++ src/main/java/solutions/ContainsDuplicate.java
@@ -1,2 +1,36 @@
-package solutions;public class ContainsDuplicate {
+package solutions;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int [] nums){
+        int n = nums.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums [j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean containsDuplicateSet(int [] nums){
+        Set<Integer> numbers = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numbers.contains(nums[i])){
+                return true;
+            }
+            numbers.add(nums[i]);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ContainsDuplicate().containsDuplicate(
+                new int[]{1, 3, 4, 1}));
+
+        System.out.println(new ContainsDuplicate().containsDuplicateSet(
+                new int[]{1, 3, 4, 1}));
+    }
 }
